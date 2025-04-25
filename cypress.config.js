7602438561cf8da850e0a39230d90338877b48b8
Nate Holder <natholder01@gmail.com>
2025-04-25 18:33:08 -0400

    Comment out projectId and reporter settings in Cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,15 +2,15 @@ const { defineConfig } = require("cypress");
 // const webpackConfig = require('./cypress/webpack.config.js');
 
 module.exports = defineConfig({
-  projectId: '4af4fb86-d18d-400f-bc6f-b2ee29582448',
-  reporter: 'spec', // Change to a simple reporter for now
+//  projectId: '4af4fb86-d18d-400f-bc6f-b2ee29582448',
+//  reporter: 'spec', // Change to a simple reporter for now
   e2e: {
     baseUrl: 'https://aretialliance.com', // Point to main website
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
-    supportFile: "cypress/support/e2e.js",
-    viewportWidth: 1280,
-    viewportHeight: 720,
-    video: false,
+//    supportFile: "cypress/support/e2e.js",
+//    viewportWidth: 1280,
+//    viewportHeight: 720,
+//    video: false,
     screenshotOnRunFailure: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
